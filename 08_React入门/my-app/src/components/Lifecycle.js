4aa79ad3b2d91479eece41e17ed8ef69575c5d52
LuckRain <763839938@qq.com>
2020-03-19 12:39:44 +0800

    'react'

--- 08_React入门/my-app/src/components/Lifecycle.js
+++ 08_React入门/my-app/src/components/Lifecycle.js
@@ -7,13 +7,11 @@ export default class Lifecycle extends Component {
     
   }
 
-
   componentDidMount() {
     // 组件已挂载，可进行状态更新操作
     console.log('3.组件已挂载')
   }
 
-  
   shouldComponentUpdate() {
     // 组件是否需要更新，需要返回布尔值结果，优化点
     console.log('5.组件是否需要更新？')
@@ -28,6 +26,7 @@ export default class Lifecycle extends Component {
     // 组件将要卸载, 可做清理工作
     console.log('8.组件将要卸载')
   }
+
   render() {
     console.log('2、组件渲染')
     return <div>生命周期探究</div>
